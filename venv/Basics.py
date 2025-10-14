c68120c6f861cc7bedbda3df958cdd09f3e2c084
Por Levente <porleventeferenc@gmail.com>
2025-10-14 22:17:39 +0200

    init venv

--- venv/Basics.py
+++ venv/Basics.py
@@ -1,1 +1,17 @@
-# opencv installing...
+import cv2
+import numpy as np
+import face_recognition
+#venv - virtual environment
+
+# loading images to compare
+imgFrusciante = face_recognition.load_image_file('ImageBasic/frusciante.jpg')
+# converting pics from BGR to RGB
+imgFrusciante = cv2.cvtColor(imgFrusciante, cv2.COLOR_BGR2RGB)
+
+imgTest = face_recognition.load_image_file('ImageBasic/fruscianteTest.jpg')
+imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
+
+# opens the image in a new window
+cv2.imshow('Frusciante', imgFrusciante)
+cv2.imshow('Frusciante Test', imgTest)
+cv2.waitKey(0) # waits until a key is pressed
