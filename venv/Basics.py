0f4bd07b35e3a12d0f6ea8026ec763d0bf47bb9a
Por Levente <porleventeferenc@gmail.com>
2025-12-01 19:38:31 +0100

    face loc

--- venv/Basics.py
+++ venv/Basics.py
@@ -15,3 +15,14 @@ imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
 cv2.imshow('Frusciante', imgFrusciante)
 cv2.imshow('Frusciante Test', imgTest)
 cv2.waitKey(0) # waits until a key is pressed
+
+# finding the location of the face in the image
+faceLoc = face_recognition.face_locations(imgFrusciante)[0]
+encodeFrusciante = face_recognition.face_encodings(imgFrusciante)[0]
+cv2.rectangle(imgFrusciante, (faceLoc[3], faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
+# faceLoc returns a list of tuples (top, right, bottom, left)
+# cv2.rectangle(image, start_point, end_point, color, thickness)
+
+faceLocTest = face_recognition.face_locations(imgTest)[0]
+encodeFruscianteTest = face_recognition.face_encodings(imgTest)[0]
+cv2.rectangle(imgTest, (faceLocTest[3], faceLocTest[0]), (faceLocTest[1], faceLocTest[2]), (255, 0, 255), 2)
